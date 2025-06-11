f450cea4011c650f79d3774cbb6d5b37025605fb
Whslv <vladon19977@gmail.com>
2025-06-11 17:28:47 -0400

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const li = document.querySelectorAll('li');
+const mainImage = document.querySelector('#largeImg');
+
+li.forEach((image) => {
+  image.addEventListener('click', (im) => {
+    im.preventDefault();
+
+    const imageLink = im.target.closest('a');
+
+    mainImage.src = imageLink.href;
+  });
+});
