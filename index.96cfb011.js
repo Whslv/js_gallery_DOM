960f24858ef015a559bbd1eca43ddffa31ee89f4
Whslv <vladon19977@gmail.com>
2025-06-11 17:29:14 -0400

    make build

---
+++ index.96cfb011.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll("li"),r=document.querySelector("#largeImg");e.forEach(function(e){e.addEventListener("click",function(e){e.preventDefault();var t=e.target.closest("a");r.src=t.href})});
+//# sourceMappingURL=index.96cfb011.js.map
